32b1f1617f8e1f9f426c3389c21b2664509f1089
Murad Gapurovich <darkkukri0550@gmail.com>
2022-11-12 16:17:22 +0300

    Form

--- resources/views/offers/show.blade.php
+++ resources/views/offers/show.blade.php
@@ -21,6 +21,17 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body card-body-light">
+                        <form action="">
+                            <input type="text" name="title" placeholder="Link name" class="form-control mb-2">
+                            <select name="type" id="" class="form-control mb-2">
+                                <option value="cpa">CPA</option>
+                                <option value="revshare">RevShare</option>
+                            </select>
+                            <select name="host" id="" class="form-control mb-2">
+                                <option value="urltrack.net">urltrack.net</option>
+                            </select>
+                            <textarea name="comment" id="" cols="30" rows="4" placeholder="Comment" class="form-control mb-2"></textarea>
+                        </form>
                     </div>
                 </div>
             </div>
