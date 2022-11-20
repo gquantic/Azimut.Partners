67e3060be3453fdf5153fa6aa5ffb732cd72a89d
Murad <darkkukri0550@gmail.com>
2022-11-21 02:15:30 +0300

    Main link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ route('home') }}">
                     <img src="{{ asset('img/azimut.svg') }}" alt="" width="100px" style="width: 100px !important;">
                 </a>
 
